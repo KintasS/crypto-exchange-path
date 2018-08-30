467ec43b151238b5454874a4b460c0279c37336f
KintasS <squintanillav@gmail.com>
2018-08-30 15:50:28 +0200

    Changes not reported from previous commit: - "TradePair" table is now manually loaded from a file instead of from Provider API - "Coin" table. Added new 'price_id' field to fetch prices from provider. The 'ID' field no longer matches the ID of the provider. "update_prices()". Updated to receive 'price_id' as imput and then transfor provider IDs in application ID before saving - "CNY" removed from calc coins, as currently there are no exchanges in the platform that support that currency. - "calc_withdraw_fee" function implemented. Now 'Percentage' fees are also considered for withdrawals. -  Minor changes in look&feel

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from crypto_exchange_path import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(debug=True, host='0.0.0.0')
+    # app.debug = True
+    # app.host = '0.0.0.0'
+    # app.run()
+    app.run(debug=True, host='0.0.0.0')
