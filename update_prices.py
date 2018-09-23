5f1531c45389cebe0def97ca6f9461bf1c07471f
KintasS <squintanillav@gmail.com>
2018-09-23 11:20:51 +0200

    Fixed error when updating prices

--- update_prices.py
+++ update_prices.py
@@ -9,7 +9,7 @@ from urllib.request import urlopen
 
 try:
     # urlopen("http://192.168.1.137:5000/update/slfjh23hk353mh4567df")
-    urlopen("http://sergioqv.pythonanywhere.com/update/slfjh23hk353mh4567df")
+    urlopen("https://www.cryptofeesaver.com/update/prices_slfjh23hk353mh4567df")
     print("ok")
 except Exception as e:
     print("KO - Error opening link")
